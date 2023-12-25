49129507894bed185269b5b780de6d289a677341
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-12-25 23:46:33 +0100

    Added number values as a objects

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
     }
     return newDice;
   }
@@ -17,7 +17,7 @@ export default function App() {
     setDice(newDice);
   }
 
-  const diceElements = dice.map((die) => <Die value={die} />);
+  const diceElements = dice.map((die) => <Die value={die.value} />);
 
   return (
     <main>
